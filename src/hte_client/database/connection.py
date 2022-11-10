d1b066ef77a1ba3a757221bab127b3ea2d3c19b6
Michael Statt <michael.statt@modelyst.io>
2022-11-10 12:58:34 -0500

    fix: urllib parsing of paswords

--- src/hte_client/database/connection.py
+++ src/hte_client/database/connection.py
@@ -55,7 +55,7 @@ class Connection(BaseModel):
         return cls(
             host=uri.host or "localhost",
             user=uri.user,
-            password=urllib.parse.urlencode(password.get_secret_value()) if password else uri.password,
+            password=urllib.parse.quote(password.get_secret_value()) if password else uri.password,
             port=uri.port or 5432,
             database=uri.path.lstrip("/"),
             schema=schema,
